25d814e97f4f96b438d3be122f37108df5783b19
VishvaS1999 <gvishva2601@gmail.com>
2023-01-06 17:05:35 +0530

    Ability to create Stack

---
+++ src/com/stackAndQueue/Stack_UC1.java
@@ -0,0 +1,13 @@
+package com.stackAndQueue;
+
+public class Stack_UC1
+{
+    public static void main(String[] args)
+    {
+        StackClass obj = new StackClass();
+        obj.push(70);
+        obj.push(30);
+        obj.push(56);
+        obj.Print();
+    }
+}
