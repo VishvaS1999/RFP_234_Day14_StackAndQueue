922e0178b33125d34dc59dbfa830831c587b7581
VishvaS1999 <gvishva2601@gmail.com>
2023-01-06 17:08:50 +0530

    Abiliity to create a Queue

---
+++ src/com/stackAndQueue/Queue_UC3.java
@@ -0,0 +1,16 @@
+package com.stackAndQueue;
+
+public class Queue_UC3
+{
+    public static void main(String[] args)
+    {
+        QueueClass list = new QueueClass();
+
+        //enqueue
+        list.add(56);
+        list.add(30);
+        list.add(70);
+
+        list.Print();
+    }
+}
